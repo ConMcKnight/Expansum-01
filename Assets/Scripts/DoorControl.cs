458a7d6dccfdaacf641d3903d4eb46fa8944e292
996016 <59923081+996016@users.noreply.github.com>
2020-12-01 15:29:36 +0000

    Added Door COntrol scripts

---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+      anim = GetComponent<Animator>();  
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider col)
+    {
+anim.SetBool ("isOpen", true);
+        
+    }
+void OnTriggerExit (Collider col)
+{
+anim.SetBool("isOpen", false);
+}
+}
