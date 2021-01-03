fa10e50130c9bef4fa31ede31eeb0361529b0c42
jimpkennedy74 <57142925+jimpkennedy74@users.noreply.github.com>
2021-01-03 19:49:04 +0000

    fire and smoke

---
+++ Assets/Scripts/DropDownTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class DropDownTrigger : MonoBehaviour
+{
+   public GameObject DropDownMove;
+
+   void OnTriggerEnter(Collider other)
+   {
+       if (other.tag == "Player")
+       {
+           DropDownMove.GetComponent<Animator>().Play("DropDown");
+           this.gameObject.GetComponent<BoxCollider>().enabled = false;
+       }
+   }
+}
+
+
